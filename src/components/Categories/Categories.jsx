dc9fa6830a8f2c993fc42fa85066b915ed16e53f
DenGolivets <oxytechmusic@gmail.com>
2024-01-17 15:25:35 +0200

    add product, products, single product

---
+++ src/components/Categories/Categories.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import styles from '../../styles/Categories.module.css';
+import { NavLink } from 'react-router-dom';
+
+const Categories = ({ 
+    title,
+    products = [],
+    amount, 
+}) => {
+
+    const list = products.filter((_, i) => i < amount);
+
+    return (
+        <section className={styles.section}>
+            <h2>
+                {title}
+            </h2>
+            <div className={styles.list}>
+                {list.map(({ id, name, image }) => (
+                    <NavLink to={`/categories/${id}`} key={id} className={styles.item}>
+                        <div className={styles.image} style={{ backgroundImage: `url(${image})` }} />
+                        <h3 className={styles.title}>
+                            {name}
+                        </h3>
+                    </NavLink>
+                ))}
+            </div>
+        </section>
+    )
+}
+
+export default Categories;
